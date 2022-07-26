b20cc3cb2e33fbbb06e1b46c40a54f4b45a62589
Syahrur Ramadhan <syahrur.ramadhan@bni.co.id>
2022-07-26 13:23:45 +0700

    Masih Stuck di tampilan awal animation

---
+++ app/src/main/java/UI/Tampilan_Animation.java
@@ -0,0 +1,33 @@
+package UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.home.R;
+
+public class Tampilan_Animation extends AppCompatActivity {
+
+    ImageView logo, splashImg;
+    TextView appName;
+    LottieAnimationView lottieAnimationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tampilan_animation);
+
+        logo                = findViewById(R.id.logo);
+        splashImg           = findViewById(R.id.img);
+        appName             = findViewById(R.id.app_name);
+        lottieAnimationView = findViewById(R.id.lottie);
+
+        splashImg.animate().translationY(-1600).setDuration(1000).setStartDelay(4000);
+        logo.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+        appName.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+        lottieAnimationView.animate().translationY(1400).setDuration(1000).setStartDelay(4000);
+    }
+}
--- app/src/main/java/com/home/Home.java
+++ app/src/main/java/com/home/Home.java
@@ -1,21 +1,15 @@
 package com.home;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class Home extends AppCompatActivity {
 
